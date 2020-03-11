f6a01086295c1ebe6554c60dfaf49bb92dd5f1b9
Martin Kennedy <MartinKennedy@Lerissas-MacBook-Pro.local>
2020-03-11 12:49:06 +1100

    Started sorting into arrays

--- NFAM_data.rb
+++ NFAM_data.rb
@@ -1,267 +1,271 @@
-12 Gauge Shot	
-5.25mm Buckshot	34	1	15	0%															
+Caliber
+Ammo Type		Damage     Pen Value   Armor Damage 
 
-8.5mm Buckshot "Magnum"	44	2	26	0%															
+"12 Gauge Shot" = {	
+["5.25mm Buckshot" => [34, 1, 15]]																
 
-6.5mm Buckshot "Express"	29	3	26	0%															
+["8.5mm Buckshot Magnum" =>	[44, 2, 26]]																
 
-7mm Buckshot	32	3	26	0%														
+["6.5mm Buckshot Express" => [29, 3, 26]]																
 
-Flechette	19	28	26	0%		
-8 Darts, the armor damage is pretty high and with 8 total darts, it destroys armor very quickly
+["7mm Buckshot" => [32, 3, 26]]															
 
-12 Gauge Slugs	RIP Slug	235	0	11	100%		
-Because it has less than 20 pen, even though it has a 100% fragment chance, it can't fragment
-
-Superformance HP Slug	190	2	12	39%		
-^ This is true of all ammo under 20 pen
-
-Grizzly 40 Slug	170	12	48	12%															
-
-HP Copper Sabot Premier	160	13	46	38%																
-
-Led Slug	147	15	45	20%																
-
-Dual Sabot Slug	75	17	65	10%															
-
-Slug "Poleva-3"	130	17	40	20%															
-
-FTX Custom Lite Slug	153	18	50	10%															
-
-Slug "Poleva-6u"	140	20	50	15%																
-
-Shell With .50 BMG (Tracer)	177	23	57	5%														
-
-AP 20 Slug	164	32	65	3%															
+["Flechette" => [19, 28, 26]]			
+}
 
 
+"12 Gauge Slugs" {	
+RIP Slug	235	0	11			
 
-20 Gauge	5.6mm Buckshot	25	1	12	0%															
+Superformance HP Slug	190	2	12		
 
-6.2mm Buckshot	21	2	13	0%															
-56
-7.5mm Buckshot	24	3	14	0%															
-57
-7.3mm Buckshot	22	3	13	0%															
-58
-Devastator Slug	188	4	13	100%															
-59
-Slug "Poleva-3"	110	14	35	20%																
-60
-Star Slug	134	16	42	10%															
-61
-Slug "Poleva-6u"	120	17	40	15%																
-62
+Grizzly 40 Slug	170	12	48															
+
+HP Copper Sabot Premier	160	13	46																
+
+Led Slug	147	15	45																	
+
+Dual Sabot Slug	75	17	65															
+
+Slug "Poleva-3"	130	17	40															
+
+FTX Custom Lite Slug	153	18	50																
+
+Slug "Poleva-6u"	140	20	50																
+
+Shell With .50 BMG (Tracer)	177	23	57														
+
+AP 20 Slug	164	32	65																
+}
 
 
-9x18mm	
-SP8	67	1	2	60%																
+20 Gauge	
+5.6mm Buckshot	25	1	12															
 
-SP7	77	2	5	2%																
+6.2mm Buckshot	21	2	13																
 
-PSV	69	3	5	40%	
+7.5mm Buckshot	24	3	14																
 
-P gzh	50	5	16	25%															
+7.3mm Buckshot	22	3	13															
 
-PSO gzh	54	5	13	35%																
+Devastator Slug	188	4	13																
 
-PS gs PPO	55	6	16	25%																
+Slug "Poleva-3"	110	14	35																
 
-PRS gs	58	6	16	30%																
+Star Slug	134	16	42															
 
-PPE gzh	61	7	15	35%																
+Slug "Poleva-6u"	120	17	40																	
 
-PPT gzh	59	8	22	17%																
 
-PST gzh	50	12	26	20%																
 
-RG028 gzh	65	13	26	2%																
+9x18mm = {	
+["SP8"	=> [67,	1, 2]]																	
 
-BZT gzh	55	16	28	17%															
+["SP7"	=> [77,	2,	5]]																
 
-PBM	44	22	30	16%																
+["PSV" => [69, 3, 5]]		
 
-PMM	56	25	33	17%																
+["Pgzh" =>	[50, 5, 16]]															
 
+PSO gzh	54	5	13																
+
+PSgsPPO	55	6	16																
+
+PRS gs	58	6	16																
+
+PPE gzh	61	7	15																	
+
+PPT gzh	59	8	22																
+
+PST gzh	50	12	26																	
+
+RG028 gzh	65	13	26																
+
+BZT gzh	55	16	28															
+
+PBM	44	22	30																
+
+PMM	56	25	33																
+}
 
 
 7.62x25mm	
-LRNPC	66	5	27	35%																
+LRNPC	66	5	27																
 
-LRN	64	6	28	35%																
+LRN	64	6	28																
 
-FMJ43	60	8	29	25%															
+FMJ43	60	8	29															
 
-akbs	58	9	32	25%															
+akbs	58	9	32															
 
-P Gl	58	10	32	25%																
+P Gl	58	10	32																
 
-T Gzh (Tracer)	60	12	34	17%																
+T Gzh (Tracer)	60	12	34																
 
-Pst gzh	50	24	36	20%																
+Pst gzh	50	24	36															
 
 
 
 9x19mm	
-RIP	102	2	11	100%															
+RIP	102	2	11																
 
-PSO gzh	56	10	32	25%																
+PSO gzh	56	10	32																	
 
-Luger CCI	70	10	38	25%																
+Luger CCI	70	10	38																	
 
-Green Tracer	58	14	33	15%																
+Green Tracer	58	14	33																	
 
-PST gzh	54	18	33	15%																
+PST gzh	54	18	33																	
 
-AP 6.3	52	25	48	5%															
+AP 6.3	52	25	48															
 
 9x21mm	
-sp12	75	14	63	35%																
+sp12	75	14	63																
 
-sp11	70	16	44	30%																
+sp11	70	16	44																	
 
-sp10	49	32	46	20%																
+sp10	49	32	46																	
 
-sp13	62	36	47	20%																
+sp13	62	36	47																
 
 5.7x28 mm	
-r37f	98	8	7	100%															
+r37f	98	8	7																
 
-ss198lf	74	10	15	80%																
+ss198lf	74	10	15																
 
-r37x	81	11	14	70%																
+r37x	81	11	14																	
 
-ss197sr	62	20	22	50%																
+ss197sr	62	20	22																	
 
-ss190	57	32	43	20%																
+ss190	57	32	43																
 
-l191 (Tracer)	58	33	41	20%															
+l191 (Tracer)	58	33	41														
 
-sb193	54	35	37	20%																
+sb193	54	35	37																
 
 4.6x30 mm	
-Action SX	65	13	39	50%																
+Action SX	65	13	39																
 
-FMJ SX	43	30	41	20%															
+FMJ SX	43	30	41																
 
-Subsonic SX	41	34	46	20%															
+Subsonic SX	41	34	46																
 
-AP SX	35	47	46	10%															
+AP SX	35	47	46															
 
 
 
 9x39mm	
-sp5	68	35	52	20%															
+sp5	68	35	52																
 
-sp6	58	43	60	10%																
+sp6	58	43	60																	
 
-SPP	67	45	56	20%															
+SPP	67	45	56															
 
-BP	60	50	68	10%															
+BP	60	50	68															
 
 
 .366	
-Geksa	80	14	38	45%																
+Geksa	80	14	38																	
 
-FMJ	73	20	36	25%															
+FMJ	73	20	36														
 
-EKO	65	30	38	20%	6	6	5	2	0	0															
+EKO	65	30	38															
 
 
 5.45x39 mm	
-SP	68	11	34	45%																
+SP	68	11	34																	
 
-HP	74	11	20	35%															
+HP	74	11	20																
 
-PRS	60	14	29	30%																
+PRS	60	14	29																	
 
-US	65	15	34	10%																
+US	65	15	34																
 
-FMJ	54	20	30	25%																
+FMJ	54	20	30															
 
-T (Tracer)	57	20	38	16%																
+T (Tracer)	57	20	38																
 
-PS	50	25	35	40%																
+PS	50	25	35																
 
-PP	46	30	32	17%																
+PP	46	30	32																
 
-BP	48	32	41	16%																
+BP	48	32	41																
 
-BT (Tracer)	44	37	49	16%																
+BT (Tracer)	44	37	49																
 
-BS	40	51	57	17%															
+BS	40	51	57														
 
-7n39	37	62	60	2%																
+7n39	37	62	60															
 
 
 
 5.56x45 mm	
-Warmage	85	3	14	90%																
+Warmage	85	3	14																
 
-55_HP	75	9	22	70%																
+55_HP	75	9	22																
 
-MK_255_Mod_0	60	17	32	3%																
+MK_255_Mod_0	60	17	32																
 
-M856 (Tracer)	55	23	34	33%																
+M856 (Tracer)	55	23	34																	
 
-FMJ	52	24	33	50%																
+FMJ	52	24	33																
 
-M855	49	26	37	40%																
+M855	49	26	37															
 
-M856A1 (Tracer)	51	37	52	33%																
+M856A1 (Tracer)	51	37	52															
 
-M855A1	44	40	52	34%																
+M855A1	44	40	52															
 
-M995	41	53	58	32%																
+M995	41	53	58																
 
 
 
 7.62x39 mm	
-HP	83	15	35	26%															
+HP	83	15	35																
 
-US	56	29	42	8%															
+US	56	29	42															
 
-T45M (Tracer)	62	31	46	12%															
+T45M (Tracer)	62	31	46														
 
-PS	54	34	52	25%															
+PS	54	34	52															
 
-BP	58	47	63	12%															
+BP	58	47	63														
 
 
 
 7.62x51 mm	
-Ultra Nosler	102	15	20	70%															
+Ultra Nosler	102	15	20															
 
-BPZ FMJ	88	31	33	25%																
+BPZ FMJ	88	31	33															
 
-TPZ SP	60	36	40	20%															
+TPZ SP	60	36	40															
 
-M80	80	41	66	17%															
+M80	80	41	66														
 
-M62 (Tracer)	79	54	75	14%															
+M62 (Tracer)	79	54	75														
 
-M61	70	68	83	13%														
+M61	70	68	83													
 
 
 
 7.62x54R	
-T-46M (Tracer)	82	41	83	18%															
+T-46M (Tracer)	82	41	83														
 
-LPS_Gzh	81	42	78	18%																
+LPS_Gzh	81	42	78																
 
-7N1	86	45	84	8%															
+7N1	86	45	84															
 
-7BT1 (Tracer)	78	59	87	8%															
+7BT1 (Tracer)	78	59	87															
 
-SNB	75	62	87	8%														
+SNB	75	62	87														
 
-7n37	72	70	88	8%															
+7n37	72	70	88															
 
 
 
 12.7x55 mm	
-ps12a	165	10	22	70%															
+ps12a	165	10	22														
 
-ps12	115	28	60	30%														
+ps12	115	28	60														
 
-ps12b	102	46	57	30%
+ps12b	102	46	57
